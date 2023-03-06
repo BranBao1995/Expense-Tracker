21b53921b8b294b288d30fdc884953f38f148a59
BranBao1995 <brandon.0702@hotmail.com>
2023-03-05 21:17:17 -0500

    created expense model, made first migration

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator,EmailValidator
 
 # Create your models here.
+
+
+class Expense(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(auto_now=True) 
+    amount = models.IntegerField(validators=[MinValueValidator(0)]) 
+    description = models.TextField()
+
