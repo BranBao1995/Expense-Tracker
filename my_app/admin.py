ae1e6b2cce4c1285a9d6e8af8bb56b50ab95c42a
BranBao1995 <brandon.0702@hotmail.com>
2023-04-17 22:37:43 -0400

    added admin portal customization

--- my_app/admin.py
+++ my_app/admin.py
@@ -4,8 +4,13 @@ from my_app.models import Expense, User
 
 # Register your models here.
 
+# Customize admin area
 class ExpenseAdmin(admin.ModelAdmin):
-    pass
+    fields = (('title', 'date'), 'amount', 'description', 'author') # customize fields in the detail page
+    list_display = ('title', 'date', 'amount') # add columns on the main model page.
+    list_filter = ('title', 'date', 'amount') # add custom filter
+    ordering = ('-date',) # order by date, from newest to oldest. Remove '-' if you want to order by date from oldest to newest.
+    search_fields = ('title', 'date')
 
 admin.site.register(Expense, ExpenseAdmin)
 
